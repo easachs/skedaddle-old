2ac9c65f8572e80c13525fb52647539da8ad6d84
Eli Sachs <easachs13@gmail.com>
2022-12-28 00:43:27 -0700

    Test: add facades for monolith

---
+++ spec/facades/parks_facade_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'ParksFacade' do
+  it 'returns instances of parks', vcr: 'denver_parks' do
+    parks = ParksFacade.parks_near('Denver')
+    expect(parks).to be_a(Array)
+    expect(parks).to be_all(ParkPoro)
+    expect(parks.length).to eq(3)
+  end
+
+  it 'errors gracefully', vcr: 'bad_parks' do
+    parks = ParksFacade.parks_near('')
+    expect(parks).to eq([])
+  end
+end
