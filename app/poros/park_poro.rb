984f9b4a29920e12ca0c597be2ce73b2c2b5945c
Eli Sachs <easachs13@gmail.com>
2023-01-09 00:41:09 -0700

    Debug: fix Park PORO description/directions

--- app/poros/park_poro.rb
+++ app/poros/park_poro.rb
@@ -18,17 +18,23 @@ class ParkPoro
     @city = nil_check(data[:city])
     @state = nil_check(data[:state])
     @country = nil_check(data[:country])
-    @description = nil_check(data[:description])
-    @directions = nil_check(data[:directions])
+    @description = clean_string(nil_check(data[:description]))
+    @directions = clean_string(nil_check(data[:directions]))
     @lat = nil_check(data[:lat])
     @lon = nil_check(data[:lon])
     @activities = data[:activities] ? data[:activities].keys.map(&:to_s) * ', ' : 'Not found'
     @url = data[:activities] ? data[:activities].values.first[:url] : 'Not found'
-    @thumbnail = data[:activities] ? data[:activities].values.first[:thumbnail] : 'Not found'
+    @thumbnail = data[:activities] ? nil_check(data[:activities].values.first[:thumbnail]) : 'Not found'
   end
 
   def nil_check(attribute)
     ['', nil].include?(attribute) ? 'Not found' : attribute
+  end
+
+  def clean_string(string)
+    string.gsub(
+      Regexp.union('&lt;br', '&lt;', '&gt;', '&amp;gt;', '&amp;', '&amp;lt;', '&#39;', '&quot;', 'BR', '<br />'), ''
+    )
   end
 
   def serialized
